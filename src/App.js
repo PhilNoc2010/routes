66bc80b9950c0049fee3b39c198a63f0834e2ae3
Phil Nocerini <nocerini@gmail.com>
2023-10-31 17:14:16 -0400

    Tuesday Practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Routes, Route, Link} from "react-router-dom";
+import { useParams } from 'react-router';
 
 function App() {
+  const Home = (props) => {
+    return (
+      <h1>Welcome Home</h1>
+    )}
+
+  const Flavor = (props) => {
+    const { flavor } = useParams()
+
+    return <h3> {isNaN(flavor) ?
+      `Your word is: ${flavor}` :
+      `Your number is: ${flavor}`}</h3>
+
+  }
+
+  const DecoratedFlavor = (props) => {
+    const {flavor, textColor, bgColor} = useParams()
+    return (
+      <div style={{backgroundColor: bgColor, color: textColor}}>
+        <h3>{isNaN(flavor) ?
+              `Your word is: ${flavor}` :
+              `Your number is: ${flavor}`}</h3>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Routing Example</h1>
+      <Routes>
+        <Route path="/home" element={<Home /> }/>
+        <Route path="/:flavor" element={<Flavor />}/>
+        <Route path="/:flavor/:textColor/:bgColor" element={<DecoratedFlavor />} />
+      </Routes>
+
     </div>
   );
 }
